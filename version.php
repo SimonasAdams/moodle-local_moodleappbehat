578a8f183cadf9f1e66a3c99b7f1a03a40c0e3b2
Noel De Martin <noel@moodle.com>
2023-10-16 11:04:02 +0200

    MOBILE-4362 behat: Fix tests

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023101100;
+$plugin->version = 2023101600;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '4.3.0';
